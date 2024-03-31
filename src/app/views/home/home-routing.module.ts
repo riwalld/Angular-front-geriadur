3ac22dfb8df92ca9c07d4bd50b2a82f046f98fce
Kerbetth <kerbetth@protonmail.com>
2024-03-31 13:31:08 +0200

    add home view

--- src/app/views/home/home-routing.module.ts
+++ src/app/views/home/home-routing.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { HomeContentComponent } from '../home-content/home-content.component';
-import { SessionGameComponent } from '../session-game/session-game.component';
 import { WordstemsComponent } from '../wordstems/wordstems.component';
 import { SourcesComponent } from 'src/app/sources/sources.component';
+import { ThemeGameComponent } from '../theme-game/theme-game.component';
+import { SessionGameComponent } from '../session-game/session-game.component';
 
 const routes: Routes = [
   {
     path: '', component: HomeComponent,
     children: [
       {path: '', component: HomeContentComponent},
-      {path: 'session-game', component: SessionGameComponent},
+      {path: 'game', component: ThemeGameComponent},
+      {path: 'game/session', component: SessionGameComponent},
       {path: 'wordstems', component: WordstemsComponent},
       {path: 'sources', component: SourcesComponent},
     ]
