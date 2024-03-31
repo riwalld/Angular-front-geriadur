fad8acfb8f286ba7bd80bb6a2783f4fddcffc4fe
Kerbetth <kerbetth@protonmail.com>
2024-03-31 13:31:17 +0200

    add home-content view

--- src/app/views/home-content/home-content.component.ts
+++ src/app/views/home-content/home-content.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProperName } from 'src/app/models/ProperName';
+import { ProperNounsService } from 'src/app/services/ProperNouns.service';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-home-content',
   templateUrl: './home-content.component.html',
   styles: ``
 })
-export class HomeContentComponent {
+export class HomeContentComponent implements OnInit {
+  nouns: ProperName[] = [];
+  isLoading!: boolean;
 
+  constructor(
+    private router: Router,
+    private properNounsService: ProperNounsService,
+    private alert: AlertService
+  ) {}
+  
+  ngOnInit(): void {
+    this.get3LastNouns();
+  }
+
+  get3LastNouns() {
+    this.isLoading = true;
+    this.properNounsService.getAll().subscribe(
+      data => {
+        this.nouns = data;
+        this.isLoading = false;
+      },
+      (err) => {
+        this.alert.alertError(err.error !== null ? err.error.message : 'Une erreur s\'est produite lors de la récupération des formateurs');
+      }
+    );
+  }
 }
