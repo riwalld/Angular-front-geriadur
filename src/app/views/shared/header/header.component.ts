705b2ba7cb65cc5316f67d90087627d9219d97ce
Kerbetth <kerbetth@protonmail.com>
2024-03-31 13:30:24 +0200

    add header

--- src/app/views/shared/header/header.component.ts
+++ src/app/views/shared/header/header.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styles: ``
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
   dashboardLink: string = "/"
-  sessiongame: string = 'session-game';
+  sessiongame: string = 'game';
   wordstems: string = 'wordstems';
   sources: string = 'sources';
+  profil: string = 'userprofile';
+  leaderboard: string = 'leaderboard';
+
+  //constructor argument picked out
+  //private authService: AuthService,
+  constructor( private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  logout() {
+    
+    Swal.fire({
+      title: 'voulez vous vraiment vous déconnecter?',
+      text: 'vous allez perdre votre session!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#0d6efd',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Oui, Poursuivre',
+      allowOutsideClick: false,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        //this.authService.logout();
+        //this.router.navigateByUrl('/public/login');
+      }
+    });
+  }
+
 }
