cc328c3d20469a63601fd7a65a7ceab303ac4a27
Kerbetth <kerbetth@protonmail.com>
2024-03-31 13:21:04 +0200

    add constants

---
+++ src/app/conf/constant.ts
@@ -0,0 +1,57 @@
+// export const URL_BASE: string = '/api';
+export const URL_BASE: string = 'http://localhost:8080';
+export const siteLanguage = 1
+export const language = 1
+
+
+export const bretonLanguage = ["breton", "breton", "brezhoneg"]
+export const cornishLanguage = ["cornish", "cornique", "kerneveureg"]
+export const englishLanguage = ["english", "anglais", "saozneg"]
+export const frenchLanguage = ["french", "français", "galleg"]
+export const gaulishLanguage = ["gaulish", "gaulois", "galianeg"]
+export const IELanguage = ["Indo-Eur.", "Indo-Eur", "Indez-Eur"]
+export const irishLanguage = ["irish", "irlandais", "iwerzhoneg"]
+export const scotLanguage = ["scottish", "écossais", "skoseg"]
+export const protoCeltLanguage = ["pr.celtic", "pr.celtic", "kt.keltiek"]
+export const welshLanguage = ["welsh", "gallois", "kembraeg"]
+
+export const brLanguage = ["br.", "br.", "br."]
+export const corLanguage = ["cor.", "cor.", "ker."]
+export const engLanguage = ["eng.", "ang.", "sa."]
+export const frLanguage = ["fr.", "fr.", "gal."]
+export const gauLanguage = ["gal.", "gau.", "gau."]
+export const indLanguage = ["pr.I.E.", "pr.I.E.", "k.I.E."]
+export const irLanguage = ["ir.", "irl.", "iw."]
+export const scLanguage = ["sc.", "eco.", "sko."]
+export const pCeltLanguage = ["pr.c.", "pr.c.", "kt.k."]
+export const welLanguage = ["wel.", "gal.", "kem."]
+
+export const nameWClass = ["name", "nom", "anv"]
+export const verbWClass = ["verb", "verbe", "verb"]
+export const adjWClass = ["adjective", "adjectif", "anv-gwan"]
+export const advWClass = ["adverb", "adverbe", "ragverb"]
+export const artWClass = ["article", "article", "doareenn"]
+export const prnWClass = ["pronoun", "pronom.", "raganv"]
+export const prepWClass = ["preposition", "préposition.", "araogenn."]
+export const conjWClass = ["conjunction", "conjonction", "stagell"]
+
+export const nameWClassAbr = ["name", "nom", "anv"]
+export const verbWClassAbr = ["vrb.", "vrb.", "vrb."]
+export const adjWClassAbr = ["adj.", "adj.", "a.-gw."]
+export const advWClassAbr = ["adv.", "adv.", "rgv."]
+export const artWClassAbr = ["art.", "art.", "doa."]
+export const prnWClassAbr = ["prn.", "prn.", "rag."]
+export const prepWClassAbr = ["prp.", "prp.", "ara."]
+export const conjWClassAbr = ["cnj.", "cnj.", "stg."]
+
+export const mGenderAbr = ["m.", "m.", "g."]
+export const fGenderAbr = ["f.", "f.", "b."]
+export const nGenderAbr = ["n.", "n.", "n."]
+export const noGenderAbr = ["n.", "n.", "n."]
+
+export const mGender = ["male", "masculin", "gourel"]
+export const fGender = ["feminine", "féminin", "benel"]
+export const nGender = ["neutral", "neutre", "neptu"]
+
+
+
